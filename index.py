d64e8407e9eee4e9a7cebe54135ad250ff18263f
Lucca465 <91845800+Lucca465@users.noreply.github.com>
2022-04-01 09:38:47 -0300

    Add index.py

---
+++ index.py
@@ -0,0 +1,13 @@
+def number_length(num):
+	#Criar o contador
+	count = 0
+
+	#Transformar o num em uma variavel string
+	string = str(num)
+
+	#Agora é possivel o loop percorrer o num que esta alocado no string
+	for i in string
+		count += 1
+
+	#Retornar o contador com a qnt de numeros obtidos no loop
+	return count
